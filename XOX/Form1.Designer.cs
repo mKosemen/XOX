b9d6040201870b4c307cef11f9380697230b5e77
mKosemen <103363649+mKosemen@users.noreply.github.com>
2022-06-11 09:20:00 +0300

    Add project files.

---
+++ XOX/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace XOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.rbX = new System.Windows.Forms.RadioButton();
+            this.rbO = new System.Windows.Forms.RadioButton();
+            this.btnYeniOyun = new System.Windows.Forms.Button();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.txtO = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.cbRakip = new System.Windows.Forms.CheckBox();
+            this.btnKurallar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(81, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(187, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(293, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(81, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(187, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(293, 124);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 3;
+            this.button6.Tag = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(81, 230);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 8;
+            this.button7.Tag = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(187, 230);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.ForeColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(293, 230);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 6;
+            this.button9.Tag = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // rbX
+            // 
+            this.rbX.AutoSize = true;
+            this.rbX.Location = new System.Drawing.Point(293, 359);
+            this.rbX.Name = "rbX";
+            this.rbX.Size = new System.Drawing.Size(32, 17);
+            this.rbX.TabIndex = 9;
+            this.rbX.TabStop = true;
+            this.rbX.Text = "X";
+            this.rbX.UseVisualStyleBackColor = true;
+            // 
+            // rbO
+            // 
+            this.rbO.AutoSize = true;
+            this.rbO.Location = new System.Drawing.Point(347, 359);
+            this.rbO.Name = "rbO";
+            this.rbO.Size = new System.Drawing.Size(33, 17);
+            this.rbO.TabIndex = 10;
+            this.rbO.TabStop = true;
+            this.rbO.Text = "O";
+            this.rbO.UseVisualStyleBackColor = true;
+            // 
+            // btnYeniOyun
+            // 
+            this.btnYeniOyun.BackgroundImage = global::XOX.Properties.Resources.temizlik;
+            this.btnYeniOyun.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnYeniOyun.Location = new System.Drawing.Point(493, 112);
+            this.btnYeniOyun.Name = "btnYeniOyun";
+            this.btnYeniOyun.Size = new System.Drawing.Size(92, 76);
+            this.btnYeniOyun.TabIndex = 11;
+            this.btnYeniOyun.UseVisualStyleBackColor = true;
+            this.btnYeniOyun.Click += new System.EventHandler(this.btnYeniOyun_Click);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.BackgroundImage = global::XOX.Properties.Resources.XOX;
+            this.btnBasla.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBasla.Location = new System.Drawing.Point(493, 30);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(92, 76);
+            this.btnBasla.TabIndex = 12;
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(78, 340);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(29, 13);
+            this.lblSkor.TabIndex = 13;
+            this.lblSkor.Text = "Skor";
+            // 
+            // txtO
+            // 
+            this.txtO.Location = new System.Drawing.Point(180, 337);
+            this.txtO.Name = "txtO";
+            this.txtO.Size = new System.Drawing.Size(32, 20);
+            this.txtO.TabIndex = 14;
+            this.txtO.Text = "0";
+            this.txtO.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(126, 337);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(32, 20);
+            this.txtX.TabIndex = 15;
+            this.txtX.Text = "0";
+            this.txtX.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.BackgroundImage = global::XOX.Properties.Resources.yenile;
+            this.btnSifirla.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSifirla.Location = new System.Drawing.Point(493, 194);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(92, 76);
+            this.btnSifirla.TabIndex = 16;
+            this.btnSifirla.UseVisualStyleBackColor = true;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // cbRakip
+            // 
+            this.cbRakip.AutoSize = true;
+            this.cbRakip.Location = new System.Drawing.Point(293, 336);
+            this.cbRakip.Name = "cbRakip";
+            this.cbRakip.Size = new System.Drawing.Size(102, 17);
+            this.cbRakip.TabIndex = 17;
+            this.cbRakip.Text = "Bilgisayara Karşı";
+            this.cbRakip.UseVisualStyleBackColor = true;
+            // 
+            // btnKurallar
+            // 
+            this.btnKurallar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKurallar.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnKurallar.Location = new System.Drawing.Point(493, 276);
+            this.btnKurallar.Name = "btnKurallar";
+            this.btnKurallar.Size = new System.Drawing.Size(92, 28);
+            this.btnKurallar.TabIndex = 18;
+            this.btnKurallar.Text = "Kurallar";
+            this.btnKurallar.UseVisualStyleBackColor = true;
+            this.btnKurallar.Click += new System.EventHandler(this.btnKurallar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(593, 405);
+            this.Controls.Add(this.btnKurallar);
+            this.Controls.Add(this.cbRakip);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.txtO);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.btnYeniOyun);
+            this.Controls.Add(this.rbO);
+            this.Controls.Add(this.rbX);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton rbX;
+        private System.Windows.Forms.RadioButton rbO;
+        private System.Windows.Forms.Button btnYeniOyun;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.TextBox txtO;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Button btnSifirla;
+        private System.Windows.Forms.CheckBox cbRakip;
+        private System.Windows.Forms.Button btnKurallar;
+    }
+}
+
---
+++ XOX/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOX
+{
+    public partial class Form1 : Form
+    {
+        int sira, xSkor = 0, oSkor = 0, hamle = 0;
+        bool tercih;
+        Button[] buttons;
+        public Form1()
+        {
+            InitializeComponent();
+            ReAktifButonlar(false);
+            rbX.Checked = true;
+        }
+        private void Butonlar(object sender, EventArgs e)
+        {
+            //rbX.Checked = rbO.Checked = false;
+            Button button = (Button)sender;
+            if (cbRakip.Checked == true)
+            {
+                if (sira % 2 == 0)
+                {
+                    button.Text = "X";
+                    button.BackgroundImage = Properties.Resources.XOX_X;
+                    sira++;
+                    PCTercih();
+                }
+                else
+                {
+                    button.Text = "O";
+                    button.BackgroundImage = Properties.Resources.XOX_O;
+                    sira++;
+                    PCTercih();
+                }
+                OyunSonu(sira);
+            }
+            if (cbRakip.Checked == false)
+            {
+                if (sira % 2 == 0)
+                {
+                    button.Text = "X";
+                    button.BackgroundImage = Properties.Resources.XOX_X;
+                    sira++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    button.BackgroundImage = Properties.Resources.XOX_O;
+                    sira++;
+                }
+                OyunSonu(sira);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons = this.Controls.OfType<Button>().ToArray();
+        }
+        private void PCTercih()
+        {
+            if (hamle < 4)
+            {
+                Random rnd = new Random();
+                int sayi = rnd.Next(4, 12);
+                if ((buttons[sayi].Text == "") && (rbO.Checked == true) && sira != 10)
+                {
+                    buttons[sayi].BackgroundImage = Properties.Resources.XOX_X;
+                    buttons[sayi].Text = "X";
+                    buttons[sayi].Enabled = false;
+                    sira++;
+                    hamle++;
+                }
+                else if ((buttons[sayi].Text == "") && (rbX.Checked == true) && sira != 9)
+                {
+                    buttons[sayi].BackgroundImage = Properties.Resources.XOX_O;
+                    buttons[sayi].Text = "O";
+                    buttons[sayi].Enabled = false;
+                    sira++;
+                    hamle++;
+                }
+                else
+                {
+                    PCTercih();
+                }
+            }
+            else if (hamle == 4)
+            {
+                OyunSonu(sira);
+            }
+        }
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            hamle = 0;
+            Temizle();
+            ReAktifButonlar(false);
+        }
+        private void BaslangicTercihi()
+        {
+            rbX.Enabled = rbO.Enabled = false;
+            button1.Enabled = button2.Enabled = button3.Enabled = button4.Enabled = button5.Enabled = button6.Enabled = button7.Enabled = button8.Enabled = button9.Enabled = true;
+        }
+        private void OyunSonu(int şira)
+        {
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button1.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button1.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button1.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button7.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button3.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button1.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button3.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button5.Text == button2.Text && button2.Text == button8.Text && button5.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button5.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "")
+            {
+                MessageBox.Show("Oyunu " + button4.Text + " kazandı.");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+            else if ((tercih == true && sira == 9) || (tercih == false && sira == 10))
+            {
+                MessageBox.Show("Oyun Berabere");
+                ReAktifButonlar(false);
+                Tabela();
+            }
+        }
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            xSkor = oSkor = 0;
+            txtX.Text = xSkor.ToString();
+            txtO.Text = oSkor.ToString();
+            ReAktifButonlar(false);
+            Temizle();
+        }
+        private void Tabela()
+        {
+            if ((tercih == true && sira % 2 == 1) || tercih == false && sira % 2 == 1)
+            {
+                xSkor++;
+                txtX.Text = xSkor.ToString();
+            }
+            else
+            {
+                oSkor++;
+                txtO.Text = oSkor.ToString();
+            }
+        }
+        private void btnKurallar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Oyunun başlamasını XOX butonu sağlar \nOyun tamamlanmadan Yeni Oyun butonuna basılırsa skorlarda değişiklik olmaz. \nSıfırla butonu tüm skorları ve oyunu sıfırlar. \nOyuna başladıktan sonra oyun modunudu değiştiremezsiniz. \nOyun Modunu değiştirmek için sıfırlama yapmak zorundasınız.\nBilgisayara Karşı oyun modu seçildiğinde oyuna siz başlarsınız");
+        }
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            if (rbX.Checked)
+            {
+                tercih = true;
+                sira = 0;
+                BaslangicTercihi();
+            }
+            else if (rbO.Checked)
+            {
+                tercih = false;
+                sira = 1;
+                BaslangicTercihi();
+            }
+            else
+            {
+                MessageBox.Show("Başlangıç tercihi yapılmadan oyuna başlanmaz.");
+            }
+        }
+        private void Temizle()
+        {
+            button1.BackgroundImage = button2.BackgroundImage = button3.BackgroundImage = button4.BackgroundImage = button5.BackgroundImage = button6.BackgroundImage = button7.BackgroundImage = button8.BackgroundImage = button9.BackgroundImage = null;
+            button1.Text = button2.Text = button3.Text = button4.Text = button5.Text = button6.Text = button7.Text = button8.Text = button9.Text = "";
+        }
+        private void ReAktifButonlar(bool aktif)
+        {
+            rbX.Enabled = rbO.Enabled = btnBasla.Enabled = true;
+            button1.Enabled = button2.Enabled = button3.Enabled = button4.Enabled = button5.Enabled = button6.Enabled = button7.Enabled = button8.Enabled = button9.Enabled = aktif;
+        }
+    }
+}
